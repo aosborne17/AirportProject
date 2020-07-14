d5daba8ba796c84b0acf0d5fa339d1a8aba7a53c
gia-bartlett <g.bartlett89@gmail.com>
2020-07-14 15:27:22 +0100

    Added helicopter class

--- rough_code/aircraft.py
+++ rough_code/aircraft.py
@@ -6,11 +6,17 @@ class Aircraft():
         self.airline = airline
 
 class Plane(Aircraft):
-    def __init__(self, model, tail_number, airline, capacity):
+    def __init__(self, model, tail_number, airline, capacity, num_rows, num_seats):
         super().__init__(model, tail_number, airline)
         self.capacity = capacity
+        self.num_rows = num_rows
+        self.num_seats = num_seats
 
 
 
-#class Helicopter(Aircraft):
+class Helicopter(Aircraft):
+    def __init__(self, model, tail_number, airline, capacity, num_seats):
+        super().__init__(model, tail_number, airline)
+        self.capacity = capacity
+        self.num_seats = num_seats
 
